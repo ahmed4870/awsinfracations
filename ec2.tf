936d0e7ff43b291f494ffdf9764b9f0ac83dee2e
your name <you@example.com>
2024-09-28 14:09:46 +0530

    files changes

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # resource block for aws 
 
 resource "aws_instance" "web" {
-    ami = "ami-08718895af4dfa033"
+    ami = "ami-06f855639265b5541"
     instance_type = "t2.micro"
     tags = {
       Name = "web-server"
